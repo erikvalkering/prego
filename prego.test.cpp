569e1870c790fca68246adad5eabb36bf9d1cece
Erik Valkering <erikvalkering@gmail.com>
2023-12-29 19:52:49 +0100

    Removed TODO about renaming "computed"

--- prego.test.cpp
+++ prego.test.cpp
@@ -224,7 +224,6 @@ auto test_dynamic_reactions() {
     assert_eq(x, true, "full_name should be queried through display_name and recompute because it was previously changed");
     assert_eq(y, true, "display_name should recompute, because nick_name changed, as well as full_name");
     assert_eq(z, false, "autorun should not react, because display_name did not change");
-    // TODO: rename computed to derived?
     // TODO: rename autorun to observe or observer or reaction or effect? or reactive?
 
     enabled.set(false);
