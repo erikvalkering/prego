a4a50884e02af946b65220faf2f697c1a7198bc0
Erik Valkering <erikvalkering@gmail.com>
2025-04-19 13:58:05 +0200

    add example from readme to integration test suite

--- tests/integration_tests.cpp
+++ tests/integration_tests.cpp
@@ -4,6 +4,7 @@
 
 #include <format>
 #include <optional>
+#include <print>
 #include <string>
 
 using namespace boost::ut;
@@ -14,6 +15,30 @@ using prego::autorun;
 using prego::calc;
 
 static suite<"integration_tests"> _ = [] {
+  "example_from_readme"_test = [] {
+    atom first_name = "John"s;
+    atom last_name = "Doe"s;
+    atom nick_name = std::optional{"Mr Unknown"s};
+
+    calc full_name = first_name + " " + last_name;
+    calc display_name = nick_name.value_or(full_name);
+
+    atom enabled = true;
+    autorun([=] {
+      if (enabled)
+        std::println("{}", display_name); // prints "Mr Unknown"
+    });
+
+    nick_name.reset();      // prints "John Doe"
+    first_name = "Jane";    // prints "Jane Doe"
+    nick_name = "Jane Doe"; // no change, nothing printed
+    first_name = "John";    // no change, nothing printed
+    enabled = false;        // autorun re-evaluated, nothing printed
+    nick_name = "John Doe"; // autorun not re-evaluated, nothing printed
+    enabled = true;         // autorun re-evaluated, prints "John Doe"
+    nick_name.reset();      // no change, nothing printed
+  };
+
   "business card"_test = [] {
     atom first_name = "John"s;
     atom last_name = "Doe"s;
